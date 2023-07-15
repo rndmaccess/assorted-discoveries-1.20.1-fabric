55aa47b30f90ae903f53e02233a27bc1f3138d75
Ryan Armstrong <ryan@localhost.localdomain>
2023-07-14 23:25:47 -0600

    Updated to 1.20.1

---
+++ src/main/java/rndm_access/assorteddiscoveries/core/ADRecipeTypes.java
@@ -0,0 +1,25 @@
+package rndm_access.assorteddiscoveries.core;
+
+import net.minecraft.recipe.Recipe;
+import net.minecraft.recipe.RecipeType;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import rndm_access.assorteddiscoveries.ADReference;
+import rndm_access.assorteddiscoveries.AssortedDiscoveries;
+import rndm_access.assorteddiscoveries.item.crafting.ADWoodcuttingRecipe;
+
+public class ADRecipeTypes {
+    public static final RecipeType<ADWoodcuttingRecipe> WOODCUTTING = register("woodcutting");
+
+    public static void registerRecipeTypes() {
+        AssortedDiscoveries.LOGGER.info("Registered recipe types.");
+    }
+
+    private static <T extends Recipe<?>> RecipeType<T> register(String path) {
+        return Registry.register(Registries.RECIPE_TYPE, ADReference.makeId(path), new RecipeType<T>() {
+            public String toString() {
+                return ADReference.makeId(path).toString();
+            }
+        });
+    }
+}
